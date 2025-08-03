d926090153a325a0d3c2dcb51eac965328922c5c
claudio.giubrone <claudiogiubrone@gmail.com>
2025-08-03 11:55:13 +0200

    imported relevant libraries

--- hyper_focus.py
+++ hyper_focus.py
@@ -1,1 +1,6 @@
 #this project aims to build an app to keep track of my project and help me improve my focus
+import sqlite3
+import datetime
+from typing import Optional, List, Dict, Any
+import json
+import os
